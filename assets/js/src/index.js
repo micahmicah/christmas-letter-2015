f57910986c3995e99d68999cff75767cdf43aff4
Micah Barrett <mbarrett02@risd.edu>
2015-12-22 17:03:04 -0800

    progress
    added:
     - slick slider
     - type

--- assets/js/src/index.js
+++ assets/js/src/index.js
@@ -1,3 +1,20 @@
 // write JavaScript here
 
-console.log($('p').text());
+// console.log($('p').text());
+
+
+            $(document).ready(function(){
+  
+			$('.story-slideshow').slick({
+  
+  dots:true,
+  speed:250,
+  cssEase: 'ease-in-out',
+   slidesToShow: 1,
+  adaptiveHeight: true
+});
+      
+});
+
+
+        
